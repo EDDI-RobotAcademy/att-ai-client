b37542f78e157439491b088e56607f6e0455189f
HoJ00n2 <81820604+HoJ00n2@users.noreply.github.com>
2024-08-20 15:52:46 +0900

    init: [이호준] 소켓 통신으로 개인 서비스를 적용합니다. [ATT-AI-1]

--- user_defined_protocol/register.py
+++ user_defined_protocol/register.py
@@ -4,6 +4,9 @@ import sys
 from first_user_defined_function_domain.service.fudf_service_impl import FudfServiceImpl
 from first_user_defined_function_domain.service.request.fudf_just_for_test_request import FudfJustForTestRequest
 from first_user_defined_function_domain.service.response.fudf_just_for_test_response import FudfJustForTestResponse
+from istp_test.service.istp_test_service_impl import IstpTestServiceImpl
+from istp_test.service.request.istp_test_request import IstpTestRequest
+from istp_test.service.response.istp_test_response import IstpTestResponse
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'template'))
 
@@ -15,8 +18,9 @@ from user_defined_protocol.protocol import UserDefinedProtocolNumber
 
 
 class UserDefinedProtocolRegister:
+
     @staticmethod
-    def registerUserDefinedProtocol():
+    def registerDefaultUserDefinedProtocol():
         customProtocolService = CustomProtocolServiceImpl.getInstance()
         firstUserDefinedFunctionService = FudfServiceImpl.getInstance()
 
@@ -36,3 +40,37 @@ class UserDefinedProtocolRegister:
             UserDefinedProtocolNumber.FIRST_USER_DEFINED_FUNCTION_FOR_TEST,
             firstUserDefinedFunctionService.justForTest
         )
+
+	# 여러분의 사용자 정의형 프로토콜 등록 파트
+    @staticmethod
+    def registerIstpTestProtocol():
+        customProtocolService = CustomProtocolServiceImpl.getInstance()
+        IstpTestService = IstpTestServiceImpl.getInstance()
+
+		# 여러분들이 구성한 것 (프로토콜과 request 등록)
+        requestClassMapInstance = RequestClassMap.getInstance()
+        requestClassMapInstance.addRequestClass(
+            UserDefinedProtocolNumber.ATT_TEAM_ISTP_TEST,
+            IstpTestRequest
+        )
+
+		# 여러분들이 구성한 것 (프로토콜과 response 등록)
+        responseClassMapInstance = ResponseClassMap.getInstance()
+        responseClassMapInstance.addResponseClass(
+            UserDefinedProtocolNumber.ATT_TEAM_ISTP_TEST,
+            IstpTestResponse
+        )
+
+		# 여러분들이 구성한 것 (프로토콜과 구동할 함수 등록)
+        customProtocolService.registerCustomProtocol(
+            UserDefinedProtocolNumber.ATT_TEAM_ISTP_TEST,
+            IstpTestService.chatWithIstp
+        )
+
+		# 초기 구동에서 호출하는 부분
+    @staticmethod
+    def registerUserDefinedProtocol():
+		# 디폴트 구성
+        UserDefinedProtocolRegister.registerDefaultUserDefinedProtocol()
+	    # 여러분의 사용자 정의형 프로토콜 등록 파트
+        UserDefinedProtocolRegister.registerIstpTestProtocol()
