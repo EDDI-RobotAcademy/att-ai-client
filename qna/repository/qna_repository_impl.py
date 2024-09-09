dc821659f71f30707e65332e82e0ef18dedeada1
HoJ00n2 <81820604+HoJ00n2@users.noreply.github.com>
2024-09-09 12:35:51 +0900

    fix: [이호준] 고도화된 응답을 위한 시스템 프롬프트 수정

--- qna/repository/qna_repository_impl.py
+++ qna/repository/qna_repository_impl.py
@@ -27,10 +27,6 @@ class QnaRepositoryImpl(QnaRepository):
         'Content-Type': 'application/json',
     }
 
-    system_prompt = {"role": "system", "content": "대화의 맥락을 요약해서 만나는 요일이 언제인지 한 개의 단어로 답해줘. "
-                                       "너는 반드시 문장으로 말하면 안되고 요일 형태로만 말해줘."
-                                       "만약 약속이 정해지지 않았다면 없다고 답해줘"}
-
     OPENAI_CHAT_COMPLETIONS_URL= "https://api.openai.com/v1/chat/completions"
 
     def __new__(cls):
@@ -47,8 +43,46 @@ class QnaRepositoryImpl(QnaRepository):
         return cls.__instance
 
     async def dateQuestion(self, userSendMessage):
+        # QnA를 위한 시스템 프롬프트
+        system_prompt = {"role": "system", "content": "Summarize the context of the conversation and answer with the day of the week in one word." 
+                                                      "You must not answer in a full sentence, only in the form of a day of the week." 
+                                                      "If the day hasn't been decided, answer 'none'." 
+                                                      "Make sure to respond in Korean."}
         # systemPrompt를 대화의 맨 앞에 추가
-        userSendMessage.insert(0, self.system_prompt)
+        userSendMessage.insert(0, system_prompt)
+        data = {
+            'model': model,
+            'messages': userSendMessage,
+            'max_tokens': 256,
+            'temperature': 0.1,
+        }
+
+        # api에 추론 요청을 보내는 부분
+        async with httpx.AsyncClient(timeout=10000) as client:
+            try:
+                response = await client.post(self.OPENAI_CHAT_COMPLETIONS_URL, headers=self.headers, json=data)
+                response.raise_for_status()
+
+                generatedText = response.json()['choices'][0]['message']['content'].strip()
+                return { "generatedText": [generatedText] } # dict 형식으로 반환해주어야 함
+
+            except httpx.HTTPStatusError as e:
+                print(f"HTTP Error: {str(e)}")
+                print(f"Status Code: {e.response.status_code}")
+                print(f"Response Text: {e.response.text}")
+                raise HTTPException(status_code=e.response.status_code, detail=f"HTTP Error: {e}")
+
+            except (httpx.RequestError, ValueError) as e:
+                print(f"Request Error: {e}")
+                raise HTTPException(status_code=500, detail=f"Request Error: {e}")
+
+    async def locationQuestion(self, userSendMessage):
+        system_prompt = {"role": "system", "content": "Summarize the context of the conversation and answer with the location where we are meeting in one word." 
+                                                      "You must not answer in a full sentence, only in word form." 
+                                                      "If the location hasn't been decided, answer 'none'." 
+                                                      "Make sure to respond in Korean."}
+        # systemPrompt를 대화의 맨 앞에 추가
+        userSendMessage.insert(0, system_prompt)
         data = {
             'model': model,
             'messages': userSendMessage,
